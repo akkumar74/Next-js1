73f8c14f49858aeabe08b2c3aa16007d0ac632f3
akkumar746 <akkumar746@gmail.com>
2023-07-20 15:58:51 +0530

    first commit

---
+++ MyApp/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+    return <>
+    <h6>Hello, santosh!</h6>;
+    <h5>Hello, santosh!</h5>;
+    <h4>Hello, Next.js!</h4>;
+    <h3>Hello, Next.js!</h3>;
+    <h2>Hello, Next.js!</h2>;
+    <h1>Hello, Next.js!</h1>;
+    </>
+    
+  }
